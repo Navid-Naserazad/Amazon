f7feb27c8cb277ff3e7f68f9e637efeca2008473
Navid-Naserazad <126415333+Navid-Naserazad@users.noreply.github.com>
2023-04-07 13:05:37 +0430

    implemented request class

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,2 +1,34 @@
 public class Request {
+    private String username;
+    private double fund;
+
+    public Request(String username, double fund)
+    {
+        this.username = username;
+        this.fund = fund;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getFund() {
+        return fund;
+    }
+
+    public void setFund(double fund) {
+        this.fund = fund;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "username='" + username + '\'' +
+                ", fund=" + fund +
+                '}';
+    }
 }
