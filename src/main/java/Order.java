d5be166bf44f531893b03e696f643a391ec2bb95
Navid-Naserazad <126415333+Navid-Naserazad@users.noreply.github.com>
2023-04-07 13:06:20 +0430

    implemented order class

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,2 +1,63 @@
+import java.util.ArrayList;
+import java.util.UUID;
+import java.time.LocalDateTime;
+
 public class Order {
+    private User user;
+    private String orderID;
+    private String date;
+    private String status = "Pending";
+    private double totalPrice;
+    private ArrayList<Product> userShoppingCart;
+
+    public Order(User user)
+    {
+        this.user = user;
+        this.orderID = UUID.randomUUID().toString();
+        this.date = java.time.LocalDate.now().toString();
+        this.totalPrice = user.calculatingTheTotalPriceOfShoppingcart();
+        this.userShoppingCart = user.getShoppingCart();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void confirmedStatus() {
+        this.status = "Confirmd";
+    }
+
+    public void canceledStatus() { this.status = "Canceled";}
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(User user) {
+        this.totalPrice = user.calculatingTheTotalPriceOfShoppingcart();
+    }
+
+    public ArrayList<Product> getUserShoppingCart() {
+        return userShoppingCart;
+    }
+
+    public void setUserShoppingCart(User user) {
+        this.userShoppingCart = user.getShoppingCart();
+    }
 }
